3fa1e28a4402983c40b5b2c3875af111073a122b
Tommy Yu <y@metatoaster.com>
2014-03-30 15:49:20 +1300

    Added a relay command.
    - Also ensure the inputs are casted into expected types.

--- mtj/jibberext/utility.py
+++ mtj/jibberext/utility.py
@@ -13,3 +13,49 @@ class Monitor(Command):
 
     def listen(self, msg=None, match=None, bot=None, **kw):
         self.items.append(msg)
+
+
+class Unping(object):
+
+    def __init__(self, s, rep='_'):
+        self.s = s
+        self.rep = rep
+
+    def __str__(self):
+        return self.s[:-1] + self.rep
+
+
+class Relay(Command):
+    """
+    Captures raw message from a source and relays to target.
+    """
+
+    def __init__(self, source, target, format_str=None):
+        """
+        Extra keywords will be passed to messages.
+        """
+
+        self.source = source
+        self.target = target
+        self.format_str = format_str
+
+    def format_msg(self, msg):
+        if not self.format_str:
+            return msg['body']
+
+        if '{mucnick_}' in self.format_str:
+            msg[u'mucnick_'] = Unping(msg.get('mucnick'))
+        return self.format_str.format(**msg)
+
+    def relay(self, msg=None, match=None, bot=None, **kw):
+        if not str(msg['from']).startswith(self.source):
+            return
+
+        new_msg = {}
+        new_msg['mto'] = self.target
+
+        if self.target in bot.muc.rooms:
+            new_msg['mtype'] = 'groupchat'
+
+        new_msg['raw'] = self.format_msg(dict(msg))
+        return new_msg
--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,6 +1,17 @@
 from unittest import TestCase
 
 from mtj.jibberext.utility import Monitor
+from mtj.jibberext.utility import Relay
+
+class FakeMuc(object):
+
+    def __init__(self, rooms):
+        self.rooms = rooms
+
+class FakeBot(object):
+
+    def __init__(self, rooms):
+        self.muc = FakeMuc(rooms)
 
 
 class MonitorTestCase(TestCase):
@@ -18,3 +29,83 @@ class MonitorTestCase(TestCase):
         monitor.listen('msg', 'match', 'bot')
         monitor.listen('msg', 'match', 'bot')
         self.assertEqual(len(monitor.items), 2)
+
+
+class RelayTestCase(TestCase):
+
+    def test_relay_setup(self):
+        relay = Relay('source@chat.example.com', 'dest@chat.example.com')
+        self.assertTrue(relay.source.startswith('source'))
+        self.assertTrue(relay.target.startswith('dest'))
+
+    def test_relay_run_not_source(self):
+        bot = FakeBot({})
+        relay = Relay('source@chat.example.com', 'dest@chat.example.com')
+        result = relay.relay(
+            msg={
+                'type': 'chat',
+                'body': 'This is a test message',
+                'from': 'elsewhere@chat.example.com',
+            },
+            match=None,
+            bot=bot,
+        )
+        self.assertIsNone(result)
+
+    def test_relay_run_pm(self):
+        bot = FakeBot({})
+        relay = Relay('source@chat.example.com', 'dest@chat.example.com')
+        result = relay.relay(
+            msg={
+                'type': 'chat',
+                'body': 'This is a test message',
+                'from': 'source@chat.example.com',
+            },
+            match=None,
+            bot=bot,
+        )
+
+        self.assertEqual(result['raw'], 'This is a test message')
+        self.assertIsNone(result.get('mtype'))
+
+    def test_relay_run_muc(self):
+        bot = FakeBot({'destroom@chat.example.com': {}})
+        relay = Relay('source@chat.example.com', 'destroom@chat.example.com')
+        result = relay.relay(
+            msg={
+                'type': 'chat',
+                'body': 'This is a test message',
+                'from': 'source@chat.example.com',
+            },
+            match=None,
+            bot=bot,
+        )
+
+        self.assertEqual(result['raw'], 'This is a test message')
+        self.assertEqual(result['mtype'], 'groupchat')
+
+    def test_format_message(self):
+        relay = Relay('source', 'dest', '{mucnick}: {body}')
+        result = relay.format_msg(
+            msg={
+                'type': 'groupchat',
+                'body': 'This is a test message',
+                'from': 'source@chat.example.com',
+                'mucnick': 'A Sender',
+            },
+        )
+
+        self.assertEqual(result, 'A Sender: This is a test message')
+
+    def test_format_message_unping(self):
+        relay = Relay('source', 'dest', '{mucnick_}: {body}')
+        result = relay.format_msg(
+            msg={
+                'type': 'groupchat',
+                'body': 'This is a test message',
+                'from': 'source@chat.example.com',
+                'mucnick': 'A Sender',
+            },
+        )
+
+        self.assertEqual(result, 'A Sende_: This is a test message')
