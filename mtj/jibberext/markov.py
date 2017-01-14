c72d787276e34198c72a59695ab6b403ab5663f5
Tommy Yu <y@metatoaster.com>
2017-01-14 16:27:34 +1300

    Rudimentary markov generation for users.

--- mtj/jibberext/markov.py
+++ mtj/jibberext/markov.py
@@ -4,7 +4,7 @@ from mtj.jibber.core import Command
 
 from mtj.markov.model import sentence
 from mtj.markov.model import xmpp
-from mtj.markov.graph.sentence import SentenceGraph
+from mtj.markov.graph.xmpp import XMPPGraph
 
 
 class Markov(Command):
@@ -13,8 +13,8 @@ class Markov(Command):
     """
 
     def __init__(self, db_src):
-        self.engine = SentenceGraph(db_src=db_src)
-        self.engine.initialize([xmpp])
+        self.engine = XMPPGraph(db_src=db_src)
+        self.engine.initialize()
 
     def learn(self, msg=None, match=None, bot=None, **kw):
         muc = msg['from'].bare
@@ -50,3 +50,45 @@ class Markov(Command):
         answer = self._generate_word(match.group('phrase'))
         if answer:
             return '%s: %s' % (msg['mucnick'], answer)
+
+    def _generate_jid(self, jid):
+        try:
+            return self.engine.generate({'jid': jid})
+        except:
+            pass
+
+    def generate_jid(self, msg, match, bot=None, **kw):
+        answer = self._generate_jid(match.group('jid'))
+        if answer:
+            return '%s: %s' % (msg['mucnick'], answer)
+
+    def mucnick_to_jid(self, muc, nick, bot):
+        info = bot.muc.rooms[muc].get(nick)
+        if info:
+            return info['jid'].bare
+
+    def _generate_nick(self, muc, nick, bot):
+        jid = self.mucnick_to_jid(muc, nick, bot)
+        if not jid:
+            return '%s is not in this chat.' % nick
+        return self._generate_jid(jid)
+
+    def generate_nick_current(self, msg, match, bot, **kw):
+        muc = msg['from'].bare
+        nick = match.group('nick')
+        answer = self._generate_nick(muc, nick, bot)
+        if answer:
+            return '%s: %s' % (msg['mucnick'], answer)
+        return '%s has never said anything before.' % nick
+
+    def generate_jid_or_nick(self, msg, match, bot, **kw):
+        muc = msg['from'].bare
+        nick = match.group('nick_or_jid')
+        jid = self.mucnick_to_jid(muc, nick, bot)
+        if jid is None:
+            jid = nick
+        answer = self._generate_jid(jid)
+        if answer:
+            return '%s: %s' % (msg['mucnick'], answer)
+        return "%s: I don't enough about someone that goes by %s." % (
+            msg['mucnick'], nick)
